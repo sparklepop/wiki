49e7a4759d3188dca51d6dcea4c156ebbe076c54
Kat Dutchin <20523663+sparklepop@users.noreply.github.com>
2022-02-02 13:00:48 -0700

    added meta for author and dates

--- app/models/wikipost.rb
+++ app/models/wikipost.rb
@@ -1,3 +1,7 @@
 class Wikipost < ApplicationRecord
     has_one_attached :image
+ 
+    def meta
+       "Created by " + self.author + " on " + I18n.l(self.created_at, format: :long) + " and last updated at " + I18n.l(self.updated_at, format: :long) 
+       end
 end
