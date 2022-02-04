354e54d8e9a782148e9ecb475e57a1152d46e9e0
Kat Dutchin <20523663+sparklepop@users.noreply.github.com>
2022-02-03 18:40:13 -0700

    attempted to update active storage for broken images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_204601) do
+ActiveRecord::Schema.define(version: 2022_02_04_011959) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
